b691b4482da0498933f642b834b4883261cfc651
qaws7791 <qaws77911@gmail.com>
2020-09-27 16:17:29 +0900

    회원가입

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 const express = require('express')
 const app = express()
 const port = 5000
+const bodyParser = require('body-parser');
+const { User } = require("./models/User");
 
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.use(bodyParser.json());
 
 const mongoose = require('mongoose')
 mongoose.connect('mongodb+srv://admin:admin@cluster0.wtbeb.mongodb.net/test?retryWrites=true&w=majority'
@@ -13,6 +18,17 @@ mongoose.connect('mongodb+srv://admin:admin@cluster0.wtbeb.mongodb.net/test?retr
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
+})
+
+app.post('/register', (req, res) => {
+
+    const user = new User(req.body)
+    user.save((err, userInfo) => {
+        if(err) return res.json({succes: false, err})
+        return res.status(200).json({
+            succes: true
+        })
+    })
 })
 
 app.listen(port, () => {
