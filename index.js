5a15b1d0f3897526b811b7e8a1dee40936129a1d
qaws7791 <qaws77911@gmail.com>
2020-09-28 15:00:47 +0900

    비밀정보 보호

--- index.js
+++ index.js
@@ -4,13 +4,13 @@ const port = 5000
 const bodyParser = require('body-parser');
 const { User } = require("./models/User");
 
+const config = require('./config/key');
 app.use(bodyParser.urlencoded({extended: true}));
 
 app.use(bodyParser.json());
 
 const mongoose = require('mongoose')
-mongoose.connect('mongodb+srv://admin:admin@cluster0.wtbeb.mongodb.net/test?retryWrites=true&w=majority'
-,{
+mongoose.connect(config.mongoURI,{
     useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false
 }).then( () => console.log('MongoDB Connected'))
 .catch(err => console.log(err))
